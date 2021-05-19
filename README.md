01b4c0f14e5ea1082c3196b8a9c47547e978aa9a
shlim1103 <gusl5525@gmail.com>
2021-05-19 16:31:27 +0900

    make useInput

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 - React Hooks Nomad Coder 강의 내용 정리 (21.05.19 ~ 21.05.20)
 - Demo:
+
+### Contents
+
+- [x] useInput
+- [ ] useTitle
+- [ ] usePageLeave
+- [ ] useClick
+- [ ] useFadeIn
+- [ ] useFullscreen
+- [ ] useHover
+- [ ] useNetwork
+- [ ] useNotification
+- [ ] useScroll
+- [ ] useTabs
+- [ ] usePreventLeave
+- [ ] useConfirm
+- [ ] useAxios
--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
-import App from "./App"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+const useInput = (initValue, validator) => {
+  const [value, setValue] = useState(initValue)
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event
+    let willUpdate = true
+    if (typeof validator === "function") {
+      willUpdate = validator(value)
+    }
+    if (willUpdate) {
+      setValue(value)
+    }
+  }
+  return { value, onChange }
+}
+
+const App = () => {
+  const maxLength = (value) => value.length < 10
+  const name = useInput("Ms.", maxLength)
+
+  return (
+    <>
+      <h1>Hello</h1>
+      <input placeholder="Input your name" {...name} />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
